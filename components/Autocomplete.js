045cd6c10f9b4735f41f9a9154ef7bbf07eea690
Pat  Pignataro <pat6284@msn.com>
2019-06-24 18:51:07 +0000

    Automatically backed up by Learn

--- components/Autocomplete.js
+++ components/Autocomplete.js
@@ -10,8 +10,35 @@ const SearchResults = require('./SearchResults');
 class Autocomplete extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
+  this.state = {
+      query: '',
+      results: resultStore.getState().results
     };
+    this.onResultStoreChange = this.onResultStoreChange.bind(this);
+    this.onSearchFieldChange = this.onSearchFieldChange.bind(this);
+  }
+
+  onResultStoreChange(newResultStoreState) {
+    this.setState({
+      results: newResultStoreState.results
+    });
+  }
+
+  onSearchFieldChange(ev) {
+    const query = ev.target.value;
+    this.setState({ query });
+
+    if (query.length > 2) {
+      actions.search(query)
+    }
+  }
+
+  componentDidMount() {
+    this.removeListener = resultStore.addListener(this.onResultStoreChange)
+  }
+
+  componentWillUnMount() {
+    this.removeListener();
   }
   render() {
     return (
