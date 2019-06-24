545bafc723a5b1e50207cff77258876146cfb18f
Pat  Pignataro <pat6284@msn.com>
2019-06-24 18:59:02 +0000

    Automatically backed up by Learn

--- stores/resultStore.js
+++ stores/resultStore.js
@@ -3,6 +3,13 @@
 const Store = require('./Store');
 
 class ResultStore extends Store {
+  constructor(initialState = {results: [], updated: new Date()}) {
+    super(initialState);
+  }
+
+   isOutdated(requestDate) {
+    return requestDate.getTime() < this.getState().updated.getTime();
+  }
 }
 
 module.exports = new ResultStore();
